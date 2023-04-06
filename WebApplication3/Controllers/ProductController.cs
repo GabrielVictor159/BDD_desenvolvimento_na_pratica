0feff2f166f3c77fc5928bcc5a7325bcfe654594
Felipe Lambert <felipe.lambert@outlook.com>
2023-04-06 18:59:49 +0000

    update configs and code lint

--- WebApplication3/Controllers/ProductController.cs
+++ WebApplication3/Controllers/ProductController.cs
@@ -10,15 +10,16 @@ namespace WebApplication3.Controllers
     {
         private ProductService _productService = new ProductService();
 
-       
-
         [HttpGet]
-        public ActionResult<Product[]> Get([FromQuery] string? nome = null, [FromQuery] string? descricao = null, [FromQuery] int id = -1, [FromQuery] double preco = -1.00, [FromQuery] string? sort = "", [FromQuery] int page = 1, [FromQuery] int size = 5)
+        public ActionResult<Product[]> Get([FromQuery] string? nome = null,
+            [FromQuery] string? descricao = null,
+            [FromQuery] int id = -1,
+            [FromQuery] double preco = -1.00,
+            [FromQuery] string? sort = "",
+            [FromQuery] int page = 1,
+            [FromQuery] int size = 5)
         {
-
             return _productService.Get(nome, descricao, id, preco, sort, page, size);
-
         }
-
     }
 }
--- WebApplication3/Repository/ProductRepository.cs
+++ WebApplication3/Repository/ProductRepository.cs
@@ -7,27 +7,26 @@ namespace WebApplication3.Repository
     {
         public Product[] products =
         {
-        new Product(1, "Copo", "Copo de vidro", 25.00d),
-        new Product(2, "Caneca", "Caneca de cerâmica", 15.00d),
-        new Product(3, "Camisa", "Camisa Azul", 30.00d),
-        new Product(4, "Garfo", "Garfo de inox", 10.00d),
-        new Product(5, "Faca", "Faca de inox", 12.00d),
-        new Product(6, "Camisa", "Vermelha", 8.00d),
-        new Product(7, "Panela", "Panela antiaderente", 50.00d),
-        new Product(8, "Frigideira", "Frigideira antiaderente", 40.00d),
-        new Product(9, "Assadeira", "Assadeira de vidro", 35.00d),
-        new Product(10, "Tigela", "Tigela de vidro", 20.00d),
-        new Product(11, "Jarra", "Jarra de vidro", 25.00d),
-        new Product(12, "Xícara", "Xícara de porcelana", 18.00d),
-        new Product(13, "Pires", "Pires de porcelana", 15.00d),
-        new Product(14, "Travessa", "Travessa de vidro", 45.00d),
-        new Product(15, "Bule", "Bule de cerâmica", 30.00d),
-        new Product(16, "Concha", "Concha de inox", 12.00d),
-        new Product(17, "Espátula", "Espátula de silicone", 10.00d),
-        new Product(18, "Escumadeira", "Escumadeira de inox", 15.00d),
-        new Product(19, "Ralador", "Ralador de inox", 20.00d),
-        new Product(20, "Cortador de pizza","Cortador de pizza de inox" ,25.00d),
-};
-
+            new Product(1, "Copo", "Copo de vidro", 25.00d),
+            new Product(2, "Caneca", "Caneca de cerâmica", 15.00d),
+            new Product(3, "Camisa", "Camisa Azul", 30.00d),
+            new Product(4, "Garfo", "Garfo de inox", 10.00d),
+            new Product(5, "Faca", "Faca de inox", 12.00d),
+            new Product(6, "Camisa", "Vermelha", 8.00d),
+            new Product(7, "Panela", "Panela antiaderente", 50.00d),
+            new Product(8, "Frigideira", "Frigideira antiaderente", 40.00d),
+            new Product(9, "Assadeira", "Assadeira de vidro", 35.00d),
+            new Product(10, "Tigela", "Tigela de vidro", 20.00d),
+            new Product(11, "Jarra", "Jarra de vidro", 25.00d),
+            new Product(12, "Xícara", "Xícara de porcelana", 18.00d),
+            new Product(13, "Pires", "Pires de porcelana", 15.00d),
+            new Product(14, "Travessa", "Travessa de vidro", 45.00d),
+            new Product(15, "Bule", "Bule de cerâmica", 30.00d),
+            new Product(16, "Concha", "Concha de inox", 12.00d),
+            new Product(17, "Espátula", "Espátula de silicone", 10.00d),
+            new Product(18, "Escumadeira", "Escumadeira de inox", 15.00d),
+            new Product(19, "Ralador", "Ralador de inox", 20.00d),
+            new Product(20, "Cortador de pizza","Cortador de pizza de inox" ,25.00d),
+        };
     }
 }
